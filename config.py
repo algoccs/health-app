ea1b994fc8fc9cafd60b1c50c69ebf31f3f071f7
algoccs <algodemoacc@gmail.com>
2026-09-26 09:25:26 -0400

    Add configuration for window dimensions and texts

---
+++ config.py
@@ -0,0 +1,36 @@
+# Dimensiones y posición de las ventanas
+WIN_WIDTH, WIN_HEIGHT = 1000, 600
+WIN_X, WIN_Y = 200, 100
+
+# Textos de MainWindow
+TXT_TITLE = 'Ruffier Test'
+TXT_HELLO = '¡Bienvenido al Programa de detección de estado de salud!'
+TXT_NEXT = 'Iniciar'
+TXT_INSTRUCTION = (
+    'Esta aplicación le permite usar la prueba de Ruffier para realizar un diagnóstico inicial de su salud.\n'
+    'La prueba de Ruffier es un conjunto de ejercicios físicos diseñado para evaluar su rendimiento cardíaco.\n\n'
+    '¿Cómo realizar la prueba en la aplicación?\n'
+    '1. Presione el botón "Iniciar" para comenzar.\n'
+    '2. En la siguiente pantalla, ingrese los datos solicitados y presione el botón correspondiente a cada etapa para iniciar el temporizador:\n'
+    '   • Use el botón "Iniciar primer test" para medir su pulso inicial en reposo.\n'
+    '   • Realice 30 sentadillas y presione "Iniciar sentadillas".\n'
+    '   • Descanse y use "Iniciar test final" para su última medición.\n'
+    '3. Ingrese sus resultados y presione "Enviar resultados" para ver su diagnóstico.\n\n'
+    '¡Importante! Si no se siente bien durante la prueba, deténgala y consulte con un médico.'
+)
+
+# Textos de TestWindow
+TXT_TEST_TITLE = 'Realizando la prueba de Ruffier'
+TXT_AGE = 'Ingrese su edad:'
+TXT_TEST1 = 'Pase 5 minutos en reposo. Haga clic en "Iniciar primer test" para medir su pulso durante 15 segundos (P1):'
+TXT_TEST2 = 'Haga clic en "Iniciar sentadillas", realice 30 sentadillas en 45 segundos y mida su pulso (P2):'
+TXT_TEST3 = 'Haga clic en "Iniciar test final", descanse 1 minuto y mida su pulso en los últimos 15 segundos (P3):'
+TXT_START_TEST1 = 'Iniciar primer test'
+TXT_START_TEST2 = 'Iniciar sentadillas'
+TXT_START_TEST3 = 'Iniciar test final'
+TXT_SEND_RESULTS = 'Enviar resultados'
+
+# Textos de ResultWindow
+TXT_RESULT_TITLE = 'Resultado del Diagnóstico'
+TXT_INDEX = 'Su índice de Ruffier es: '
+TXT_WORKOUT = 'Rendimiento cardíaco: '
